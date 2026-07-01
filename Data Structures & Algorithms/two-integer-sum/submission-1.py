f377ced8996e127da333ce02a5446eb972d08feb
jasonhuang7gh <jasonhuang7@outlook.com>
2026-07-01 15:35:36 -0700

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums_dict = {}
+        for i in range(len(nums)):
+            if target - nums[i] in nums_dict:
+                return [nums_dict[target - nums[i]], i]
+            else:
+                nums_dict[nums[i]] = i
+        
+        return [-1, -1]
