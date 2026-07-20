18e76578c607f59f48ad5b2c5ff5da408e7e62fc
jasonhuang7gh <jasonhuang7@outlook.com>
2026-07-20 16:13:49 -0700

    Add: binary-search - submission-2

---
+++ Data Structures & Algorithms/binary-search/submission-2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        # Use two pointers for lower boundary and upper boundary
+        # Then set one of the pointers to the midpoint until target is found
+        # Time: O(log(n)) / Space: O(1)
+
+        # i is the lower boundary, j is the upper boundary
+        # at the start, i is the first element and j is the last
+        i, j = 0, len(nums) - 1
+
+        while(i <= j):
+            k = (i + j) // 2
+            # if the midpoint is the target, index has been found
+            if target == nums[k]:
+                return k
+            # if target in upper half, set i as midpoint (non-inclusive)
+            elif nums[k] < target:
+                i = k + 1
+            # if target in lower half, set j as midpoint (non-inclusive)
+            else:
+                j = k - 1
+            
+        # after binary search of whole array, target is not found
+        return -1
