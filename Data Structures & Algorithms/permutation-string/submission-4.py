b5ef66f14ae666a27c4011ddd8e5fee6bd5a7147
jasonhuang7gh <jasonhuang7@outlook.com>
2026-08-31 16:02:56 -0700

    Add: permutation-string - submission-4

---
+++ Data Structures & Algorithms/permutation-string/submission-4.py
@@ -0,0 +1,46 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        # Keep a frequency map of chars in s1. For every k letters in s2, if
+        # the count of a char is exceeded, we can stop counting early and proceed
+        # to next k letters.
+        # Time: O(n * k) / Space: O(k)
+        freqs_s1 = {}
+        for char in s1:
+            if char in freqs_s1:
+                freqs_s1[char] += 1
+            else:
+                freqs_s1[char] = 1
+        
+        k = len(s1)
+        for i in range(len(s2) - k + 1):
+            if s2[i] in freqs_s1:
+                freqs_s2 = {}
+                length_s2 = 0
+                for j in range(i, i + k):
+                    char = s2[j]
+                    if char not in freqs_s1:
+                        break
+                    if char in freqs_s2:
+                        freqs_s2[char] += 1
+                    else:
+                        freqs_s2[char] = 1
+                    if freqs_s2[char] > freqs_s1[char]:
+                        break
+                    length_s2 += 1
+                    if length_s2 == k:
+                        return True
+        return False
+
+
+        # s1 has k letters. For every k letters in s2, compare sorted(s1) to
+        # sorted(s2 substring) and if equal, a permutation of s1 exists in s2.
+        # Time: O(n * klog(k)) / Space: O(k)
+        k = len(s1)
+        s1_sorted = sorted(s1)
+        for i in range(len(s2) - k + 1):
+            # Slight optimization to perform comparison only if s2[i] in s1
+            if s2[i] in s1:
+                s2_substring = s2[i : i + k]
+                if s1_sorted == sorted(s2_substring):
+                    return True
+        return False
